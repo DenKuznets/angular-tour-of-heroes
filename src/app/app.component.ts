8d3b919d28e0aba72d7583d630f7a03603e05730
Denis Kyznetsov <104186466+DenKuznets@users.noreply.github.com>
2023-12-26 09:50:15 +0300

    feat: pt1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [HeroesComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+  styleUrl: './heroes.component.scss',
 })
 export class HeroesComponent {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  };
+  heroes = HEROES;
 }
