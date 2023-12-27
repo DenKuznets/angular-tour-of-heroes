3771f74fa8cdf898c72a3670ac2aeb1fbeb4bafa
Denis Kyznetsov <104186466+DenKuznets@users.noreply.github.com>
2023-12-27 11:40:48 +0300

    feat: add dashboard route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
 import { MessagesComponent } from './messages/messages.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeroesComponent, CommonModule, RouterOutlet, MessagesComponent],
+  imports: [
+    RouterLink,
+    HeroesComponent,
+    CommonModule,
+    RouterOutlet,
+    MessagesComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
